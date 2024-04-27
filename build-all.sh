486433076a56d6857f86685e02af21606d6dd131
Romeu Junior <romeu.junior@gmail.com>
2024-04-27 14:21:31 -0300

    - Improvements in the image build scripts;

---
+++ build-all.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+VERSIONS=("22.0" "22.1" "23.0" "23.1" "23.2" "24.0" "24.0.1" "24.1" "24.2" "25.0" "25.1" "25.2" "26.0" "26.1" "27.0")
+
+for STRING in "${VERSIONS[@]}"; do
+    unset VERSION
+    export VERSION="$STRING"
+    ./build-version.sh $VERSION
+done
+
+echo ""
+echo "Clean manifest for latest version."
+docker manifest rm romeujr/bitcoin-core:latest
+
+echo ""
+echo "Create manifest for latest version."
+docker manifest create romeujr/bitcoin-core:latest romeujr/bitcoin-core:$VERSION-amd64 romeujr/bitcoin-core:$VERSION-arm64
+
+echo ""
+echo "Push manifest for latest version."
+docker manifest push romeujr/bitcoin-core:latest
---
+++ build-version.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+VERSION=$1
+
+echo ""
+echo "Clean old images for version $VERSION."
+docker image rm romeujr/bitcoin-core:$VERSION-amd64
+docker image rm romeujr/bitcoin-core:$VERSION-amd64
+echo "y" | docker builder prune
+
+echo ""
+echo "Build version $VERSION for amd64..."
+docker buildx build --platform linux/amd64 --build-arg P_TARGETPLATFORM="linux/amd64" --build-arg P_BITCOIN_VERSION="$VERSION" -t romeujr/bitcoin-core:$VERSION-amd64 -f Dockerfile .
+
+echo ""
+echo "Build version $VERSION for amd64..."
+docker buildx build --platform linux/arm64 --build-arg P_TARGETPLATFORM="linux/arm64" --build-arg P_BITCOIN_VERSION="$VERSION" -t romeujr/bitcoin-core:$VERSION-arm64 -f Dockerfile .
+
+echo ""
+echo "Push version $VERSION for amd64..."
+docker push romeujr/bitcoin-core:$VERSION-amd64
+
+echo ""
+echo "Push version $VERSION for arm64..."
+docker push romeujr/bitcoin-core:$VERSION-arm64
+
+echo ""
+echo "Clean manifest for version $VERSION."
+docker manifest rm romeujr/bitcoin-core:$VERSION
+
+echo ""
+echo "Create manifest for version $VERSION."
+docker manifest create romeujr/bitcoin-core:$VERSION romeujr/bitcoin-core:$VERSION-amd64 romeujr/bitcoin-core:$VERSION-arm64
+
+echo ""
+echo "Push manifest for version $VERSION."
+docker manifest push romeujr/bitcoin-core:$VERSION
